6507261325f09cafb578ac2a5a692700d69ed867
anjicruz <anji_cruz@msn.com>
2021-02-18 21:29:32 +0800

    Using cout 0218 929pm

--- TeachYourselfCPP/main.cpp
+++ TeachYourselfCPP/main.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
-
 int main() {
-	int x = 5;
-	int y = 7;
+	std::cout << "Hello There.\n";
+	std::cout << "The manipulator std::endl";
+	std::cout << "Writes a new line to the screen.";
 	std::cout << std::endl;
-	std::cout << x + y << " " << x*y;
+	std::cout << "Here is a very big number:\t" << 70000;
 	std::cout << std::endl;
-
+	std::cout << "Here is the sum of 8 and 5:\t";
+	std::cout << 8+5 << std::endl;
+	std::cout << "Here's a fraction:\t\t";
+	std::cout << "(float)5/8" << std::endl;
+	std::cout << "And a very big number:\t";
+	std::cout << (double)7000*7000 << std::endl;
+	std::cout << "Don't forget to replace Jesse Liberty ";
+	std::cout << "with your name...\n";
+	std::cout << "Jesse Liberty is a C++ programmer!\n";
 	return 0;
 }
