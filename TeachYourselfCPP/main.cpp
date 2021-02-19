5b7f0ef46307c9316d2cafd1958d13b31803915e
anjicruz <anji_cruz@msn.com>
2021-02-19 14:40:16 +0800

    Demonstrating a Call to a Function 0219 240pm

--- TeachYourselfCPP/main.cpp
+++ TeachYourselfCPP/main.cpp
@@ -1,23 +1,10 @@
 #include <iostream>
+void DemonstrationFUnction() {
+	std::cout << "In Demonstration Function\n";
+}
 int main() {
-	/*using std::cout;
-	using std::endl;*/
-	using namespace std;
-
-	cout << "Hello There.\n";
-	cout << "The manipulator endl";
-	cout << " writes a new line to the screen.";
-	cout << endl;
-	cout << "Here is a very big number:\t" << 70000;
-	cout << endl;
-	cout << "Here is the sum of 8 and 5:\t";
-	cout << 8+5 << endl;
-	cout << "Here's a fraction:\t\t";
-	cout << "(float)5/8" << endl;
-	cout << "And a very big number:\t";
-	cout << (double)7000*7000 << endl;
-	cout << "Don't forget to replace Jesse Liberty ";
-	cout << "with your name...\n";
-	cout << "Jesse Liberty is a C++ programmer!\n";
+	std::cout << "In Main\n";
+	DemonstrationFUnction();
+	std::cout << "Back in main\n";
 	return 0;
 }
