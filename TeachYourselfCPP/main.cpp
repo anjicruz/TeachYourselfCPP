fe234e32f16e2769a3a7f6e027bc989d6931eb06
anjicruz <anji_cruz@msn.com>
2021-02-19 15:53:07 +0800

    Subtract Function 0219 352pm

--- TeachYourselfCPP/main.cpp
+++ TeachYourselfCPP/main.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
-void DemonstrationFUnction() {
-	std::cout << "In Demonstration Function\n";
+int Subtract(int first, int second)
+{
+	std::cout << "Subtraction equals: " << first - second << "\n";
+	return (first - second);
 }
-int main() {
-	std::cout << "In Main\n";
-	DemonstrationFUnction();
-	std::cout << "Back in main\n";
+int main() 
+{
+	std::cout << "I'm on main: \n";
+	int a, b;
+	std::cin >> a;
+	std::cin >> b;
+	Subtract(a, b);
+	std::cout << "Exiting...\n";
+
 	return 0;
 }
