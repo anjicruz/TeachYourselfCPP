7f6153c92e81f7567516387fb64a55608f951ad9
anjicruz <anji_cruz@msn.com>
2021-02-21 00:00:15 +0800

    LISTING 3.1 Determining the Size of Variable Types on Your Computer 0220 1200am

--- TeachYourselfCPP/main.cpp
+++ TeachYourselfCPP/main.cpp
@@ -2,10 +2,14 @@
 
 int main() 
 {
-	unsigned short x = 10;
-	unsigned short y = 11;
-
-	unsigned short z = x*y;
-	std::cout << z << "\n";
+	using std::cout;
+	cout << "The size if an int:\t\t" << sizeof(int) << "bytes.\n";
+	cout << "The size if a short int is:\t" << sizeof(short) << " bytes.\n";
+	cout << "The size if a long int is:\t" << sizeof(long) << " bytes.\n";
+	cout << "The size if a char is:\t\t" << sizeof(char) << " bytes.\n";
+	cout << "The size if a float is:\t\t" << sizeof(float) << " bytes.\n";
+	cout << "The size if a double is:\t" << sizeof(double) << " bytes.\n";
+		cout << "The size if a bool is:\t\t" << sizeof(bool) << " bytes.\n";
+		
 	return 0;
 }
