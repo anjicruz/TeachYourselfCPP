1f730162d9aaa1d885acc23959312e3c17e10d51
anjicruz <anji_cruz@msn.com>
2021-02-22 19:32:11 +0800

    Writing Past the End of an Array 0222 731pm

--- TeachYourselfCPP/main.cpp
+++ TeachYourselfCPP/main.cpp
@@ -1,15 +1,27 @@
+// Writing Past the End of an Array
 #include <iostream>
+using namespace std;
 
 int main() 
 {
-	int myArray[5];
+	long TargetArray[25];
+
 	int i;
-	for (i = 0; i < 5; i++)
-	{
-		std::cout << "Value for myArray[ " << i << " ]: ";
-		std::cin >> myArray[i];
-	}
-	for (i = 0; i < 5; i++)
-		std::cout << i << ": " << myArray[i] << std::endl;
+	for (i = 0; i < 25; i++)
+		TargetArray[i] = 10;
+
+	cout << "\nTest 1: \n";
+	cout << "TargetArray[0]: " << TargetArray[0] << endl;
+	cout << "TargetArray[24]: " << TargetArray[24] << endl << endl;
+
+	cout << "\nAttempting at assigning value beyond the upper-bound...";
+	for (i = 0; i < 25; i++)
+		TargetArray[i] = 20;
+
+	cout << "\nTest 2: \n";
+	cout << "TargetArray[0]: " << TargetArray[0] << endl;
+	cout << "TargetArray[24]: " << TargetArray[24] << endl;
+	cout << "TargetArray[25]: " << TargetArray[25] << endl << endl;
+
 	return 0;
 }
