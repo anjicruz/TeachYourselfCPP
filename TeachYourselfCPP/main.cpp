791e4b0de56a39b9f3f4f10b9b0a521597d26123
anjicruz <anji_cruz@msn.com>
2021-02-24 12:17:39 +0800

    using cin.get() 0224 1217pm

--- TeachYourselfCPP/main.cpp
+++ TeachYourselfCPP/main.cpp
@@ -1,11 +1,11 @@
-//Listing 4.5 char array buffers
+// using cin.get()
 #include <iostream>
 
 int main() 
 {
 	char buffer[80] = { '\0' };
 	std::cout << "Enter the string: ";
-	std::cin >> buffer;
+	std::cin.get(buffer, 79);                                                            
 	std::cout << "Here's the buffer: " << buffer << std::endl;
 	return 0;
 }
