43bc8c3b5a797489b54bcc8d48e48518c6cc40ca
anjicruz <anji_cruz@msn.com>
2021-02-26 21:28:46 +0800

    demonstrates if statement with else clause 0226 928pm

--- TeachYourselfCPP/main.cpp
+++ TeachYourselfCPP/main.cpp
@@ -1,29 +1,21 @@
-// demonstrates if statement
+// demonstrates if statement with else clause
 #include <iostream>
 
 int main() 
 {
 	using std::cout;
-	using std::endl;
+	//using std::endl;
 	using std::cin;
-	int MetsScore, YankeesScore;
-	cout << "Enter the score for the Mets: ";
-	cin >> MetsScore;
-	cout << "Enter the score for the Yankees: ";
-	cin >> YankeesScore;
-	cout << endl;
-	if(MetsScore > YankeesScore)
-		cout << "Let's go Mets!\n";
-	if (MetsScore < YankeesScore) { cout << "Go Yankees!\n"; }
-	if (MetsScore == YankeesScore)
-	{
-		cout << "A tie. Nooo, cant be.\n";
-		cout << "Give me the real score for the Yanks.\n";
-		cin >> YankeesScore;
-		if (MetsScore > YankeesScore) cout << "Knew it. Let's go Mets!\n";
-		if(YankeesScore>MetsScore) cout << "Knew it. Let's go Yanks!\n";
-		if (YankeesScore == MetsScore) cout << "Wow. It really was a tie!\n";
-	}
-	cout << "Thanks for letting me know!\n";
+
+	int firstNumber, secondNumber;
+	cout << "Please enter a big number: ";
+	cin >> firstNumber;
+	cout << "\nPlease enter a smaller number: ";
+	cin >> secondNumber;
+	if (firstNumber > secondNumber)
+		cout << "\nThanks!\n\n";
+	else
+		cout << "\nThe first number is not bigger!\n\n";
+	
 	return 0;
 }
