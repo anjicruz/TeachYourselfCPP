a7171602151537c7e9e7ce3711e8b05f4f7f6f5a
anjicruz <anji_cruz@msn.com>
2021-02-26 15:48:37 +0800

    demonstrates if statement 0226 348pm

--- TeachYourselfCPP/main.cpp
+++ TeachYourselfCPP/main.cpp
@@ -1,33 +1,29 @@
-// A Demonstration of Prefix and Postfix Operators
+// demonstrates if statement
 #include <iostream>
 
 int main() 
 {
 	using std::cout;
 	using std::endl;
-
-	int myAge = 39;
-	int yourAge = 39;
-	cout << "I am: " << myAge << " years old.\n";
-	cout << "I am: " << yourAge << " years old.\n";
-	myAge++; //postfix increment
-	++yourAge; // prefix increment
-	cout << "One year passes...\n";
-	cout << "I am: " << myAge << " years old.\n";
-	cout << "I am: " << yourAge << " years old.\n";
-	cout << "Another year passes...\n";
-	cout << "I am: " << myAge++ << " years old.\n";
-	cout << "I am: " << ++yourAge << " years old.\n";
-	cout << "Let's print it again...\n";
-	cout << "I am: " << myAge << " years old.\n";
-	cout << "I am: " << yourAge << " years old.\n";
+	using std::cin;
+	int MetsScore, YankeesScore;
+	cout << "Enter the score for the Mets: ";
+	cin >> MetsScore;
+	cout << "Enter the score for the Yankees: ";
+	cin >> YankeesScore;
+	cout << endl;
+	if(MetsScore > YankeesScore)
+		cout << "Let's go Mets!\n";
+	if (MetsScore < YankeesScore) { cout << "Go Yankees!\n"; }
+	if (MetsScore == YankeesScore)
+	{
+		cout << "A tie. Nooo, cant be.\n";
+		cout << "Give me the real score for the Yanks.\n";
+		cin >> YankeesScore;
+		if (MetsScore > YankeesScore) cout << "Knew it. Let's go Mets!\n";
+		if(YankeesScore>MetsScore) cout << "Knew it. Let's go Yanks!\n";
+		if (YankeesScore == MetsScore) cout << "Wow. It really was a tie!\n";
+	}
+	cout << "Thanks for letting me know!\n";
 	return 0;
-	/*
-		DO use the prefix operator (++variable)
-		to increment or decrement the
-		variable before it is used in the expression.
-		DO use the postfix operator (variable++)
-		to increment or decrement the
-		variable after it is used.*/
-		
 }
