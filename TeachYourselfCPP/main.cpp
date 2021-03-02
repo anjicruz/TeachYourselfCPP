aa83ca9e6fd42ae9e3ecfffad93ed2b28e0f7134
anjicruz <anji_cruz@msn.com>
2021-03-03 05:22:41 +0800

    demonstrates variables scoped within a block 0303 522am

--- TeachYourselfCPP/main.cpp
+++ TeachYourselfCPP/main.cpp
@@ -1,46 +1,51 @@
-// demonstrates the use of function prototypes
+// demonstrates variables scoped within a block
 
 #include <iostream>
-int Area(int length, int width);
+void myFunc();
 
 int main() 
 {
 	using std::cout; using std::endl; using std::cin;
 
 
-	int lengthOfYard{ 0 };
-	int widthOfYard{ 0 };
-	int areaOfYard{ 0 };
+	int x = 5;
+	cout << "\nIn main x is: " << x;
 
-	cout << "\nHow wide is your yard? ";
-	cin >> widthOfYard;
-	cout << "\nHow long is your yard? ";
-	cin >> lengthOfYard;
+	myFunc();
 
-	areaOfYard = Area(lengthOfYard, widthOfYard);
-
-	cout << "Your yard is ";
-	cout << areaOfYard;
-	cout << " square feet.\n\n";
+	cout << "\nBack in main x is: " << x << endl;
 	return 0;
 }
 
-int Area(int len, int wid)
+void myFunc()
 {
-	return len * wid;
-}
+	using std::cout; using std::endl; using std::cin;
+	int x = 8;
+	cout << "\nIn myFunc, local x: " << x << endl;
+	{
+		cout << "\nIn block in myFunc, x is: " << x;
 
-//The prototype for the Area() function is on line 4. Compare the prototype with the definition
-//of the function on line 28. Note that the name, the return type, and the parameter
-//types are the same.If they were different, a compiler error would have been generated.
-//In fact, the only required difference is that the function prototype ends with a semicolon
-//and has no body.
-//Also note that the parameter names in the prototype are length and width, but the parameter
-//names in the definition are len and wid.As discussed, the names in the prototype
-//are not used; they are there as information to the programmer.It is good programming
-//practice to match the prototype parameter names to the implementation parameter names,
-//but as this listing shows, this is not required.
-//The arguments are passed in to the function in the order in which the parameters are
-//declared and defined, but no matching of the names occurs.Had you passed in
-//widthOfYard, followed by lengthOfYard, the FindArea() function would have used the
-//value in widthOfYard for length and lengthOfYard for width.
+		int x = 9;
+
+		cout << "\nVery local x: " << x;
+	}
+
+	cout << "\nOut of block, in myFunc, x:" << x << endl;
+}
+//This program begins with the initialization of a local variable, x, on line 10, in main().
+//The printout on line 11 verifies that x was initialized with the value 5. On line 13,
+//MyFunc() is called.
+//On line 21 within MyFunc(), a local variable, also named x, is initialized with the value
+//8. Its value is printed on line 22.
+//The opening brace on line 24 starts a block.The variable x from the function is printed
+//again on line 25. A new variable also named x, but local to the block, is created on line
+//27 and initialized with the value 9. The value of this newest variable x is printed on line
+//29. The local block ends on line 30, and the variable created on line 27 goes out of scope
+//and is no longer visible.
+//When x is printed on line 32, it is the x that was declared on line 21 within myFunc().
+//This x was unaffected by the x that was defined on line 27 in the block; its value is still 8.
+//On line 33, MyFunc() goes out of scope, and its local variable x becomes unavailable.
+//Execution returns to line 14. On line 15, the value of the local variable x, which was created
+//on line 10, is printed.It was unaffected by either of the variables defined in
+//MyFunc().Needless to say, this program would be far less confusing if these three variables
+//were given unique names!
