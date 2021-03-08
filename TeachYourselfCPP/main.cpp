c8b883b97d562904bad94053a7819d2043e7e528
anjicruz <anji_cruz@msn.com>
2021-03-08 08:24:04 +0800

    while Loops 0308 823am

--- TeachYourselfCPP/main.cpp
+++ TeachYourselfCPP/main.cpp
@@ -1,4 +1,4 @@
-﻿// Looping with the Keyword goto
+﻿// while Loops
 
 #include <iostream>
 
@@ -6,23 +6,21 @@ int main()
 {
 	using std::cout; using std::endl; using std::cin;
 	int counter = 0;
-loop:
-	counter++;
-	cout << "Counter: " << counter << endl;
-	if (counter < 5)
-		goto loop;
 
+	while (counter < 5)
+	{
+		counter++;
+		cout << "Counter: " << counter << endl;
+	}
 	cout << "Complete. Counter: " << counter << endl;
 	return 0;
 }
-//On line 8, counter is initialized to zero.A label called loop is on line 9, marking the top
-//of the loop.counter is incremented and its new value is printed on line 11. The value of
-//counter is tested on line 12. If the value is less than 5, the if statement is true and the
-//goto statement is executed.This causes program execution to jump back to the loop
-//label on line 9. The program continues looping until counter is equal to 5, at which time
-//it “falls through” the loop and the final output is printed.
-//Why goto Is Shunned
-//As a rule, programmers avoid goto, and with good reason. goto statements can cause a
-//jump to any location in your source code, backward or forward.The indiscriminate use
-//of goto statements has caused tangled, miserable, impossible - to - read programs known as
-//spaghetti code.
+//Analysis ▼
+//This simple program demonstrates the fundamentals of the while loop.On line 8, an
+//integer variable called counter is created and initialized to zero.This is then used as a
+//part of a condition.The condition is tested, and if it is true, the body of the while loop is
+//executed.In this case, the condition tested on line 10 is whether counter is less than 5. If
+//the condition is true, the body of the loop is executed; on line 12, the counter is incremented,
+//and on line 13, the value is printed.When the conditional statement on line 10
+//fails(when counter is no longer less than 5), the entire body of the while loop(lines
+//11–14) is skipped.Program execution falls through to line 15.
