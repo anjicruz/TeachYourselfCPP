d9ca4201903fe54e8f5aa2c3baca76a8ac89364e
anjicruz <anji_cruz@msn.com>
2021-03-09 08:17:23 +0800

    Complex while Loops 0309 817am

--- TeachYourselfCPP/main.cpp
+++ TeachYourselfCPP/main.cpp
@@ -1,26 +1,45 @@
-﻿// while Loops
+﻿// Complex while Loops
 
 #include <iostream>
 
 int main() 
-{
+{	
 	using std::cout; using std::endl; using std::cin;
-	int counter = 0;
+	unsigned short small;
+	unsigned long large;
+	const unsigned short MAXSMALL = 65535;
 
-	while (counter < 5)
+	cout << "Enter a small number: ";
+	cin >> small;
+	cout << "Enter a large number: ";
+	cin >> large;
+
+	cout << "small: " << small << "...";
+
+
+	while (small<large&&small<MAXSMALL)
 	{
-		counter++;
-		cout << "Counter: " << counter << endl;
+		if (small % 5000 == 0)
+			cout << ".";
+
+		small++;
+		large -= 2;
 	}
-	cout << "Complete. Counter: " << counter << endl;
+
+	cout << "\nSmall: " << small << " Large: " << large << endl;
 	return 0;
 }
 //Analysis ▼
-//This simple program demonstrates the fundamentals of the while loop.On line 8, an
-//integer variable called counter is created and initialized to zero.This is then used as a
-//part of a condition.The condition is tested, and if it is true, the body of the while loop is
-//executed.In this case, the condition tested on line 10 is whether counter is less than 5. If
-//the condition is true, the body of the loop is executed; on line 12, the counter is incremented,
-//and on line 13, the value is printed.When the conditional statement on line 10
-//fails(when counter is no longer less than 5), the entire body of the while loop(lines
-//11–14) is skipped.Program execution falls through to line 15.
+//This program is a game.Enter two numbers, one small and one large.The smaller number
+//will count up by ones, and the larger number will count down by twos.The goal of
+//the game is to guess when they’ll meet.
+//On lines 12–15, the numbers are entered.Line 20 sets up a while loop, which will continue
+//only as long as two conditions are met :
+//1. small is not bigger than large.
+//2. small doesn’t overrun the size of a small integer(MAXSMALL).
+//On line 22, the value in small is calculated modulo 5, 000. This does not change the
+//value in small; however, it returns the value 0 only when small is an exact multiple of
+//5, 000. Each time this happens, a dot(.) is printed to the screen to show progress.On line
+//25, small is incremented, and on line 26, large is decremented by 2. When either of the
+//two conditions in the while loop fails, the loop ends and execution of the program continues
+//after the while loop’s closing brace on line 27.
