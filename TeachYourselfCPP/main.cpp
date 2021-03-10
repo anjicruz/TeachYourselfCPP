c1663bc34f9b0a9afaf34139ab94b5b43b73ff99
anjicruz <anji_cruz@msn.com>
2021-03-10 10:53:20 +0800

    while Reexamined - Looping with while 0310 1053am

--- TeachYourselfCPP/main.cpp
+++ TeachYourselfCPP/main.cpp
@@ -1,35 +1,43 @@
-﻿//	Demonstrating a do...while Loop
+﻿//	while Reexamined - Looping with while
 
 #include <iostream>
 
 
-
 int main() 
 {	using std::cout; using std::endl; using std::cin;
-	int counter;
-	cout << "How many hellos?: ";
-	cin >> counter;
-	do
+	int counter=0;
+
+	while (counter < 5)
 	{
-		cout << "Hello!\n";
-		counter--;
-	} while (counter > 0);
-	cout << "Counter is Output: " << counter << endl;
+		counter++; // commenting this line does an endless loop
+		cout << "Looping!   ";
+	}
+
+	cout << "\nCounter: " << counter << endl;
 	return 0;
-}/*
-Output ▼
-How many hellos ? 2
-Hello
-Hello
-Counter is : 0
-Analysis ▼
-Like the previous program, Listing 7.7 prints the word Hello to the console a specified
-number of times.Unlike the preceding program, however, this program will always print
-at least once.
-The user is prompted for a starting value on line 10, which is stored in the integer variable
-counter.In the do...while loop, the body of the loop is entered before the condition
-is tested, and, therefore, the body of the loop is guaranteed to run at least once.On
-line 14, the hello message is printed; on line 15, the counter is decremented; execution
-jumps to the top of the loop on line 13—otherwise, it falls through to line 17.
-The continue and break statements work in a do...while loop exactly as they do in a
-while loop.The only difference*/
+}
+//Output ▼
+//Looping!Looping!Looping!Looping!Looping!
+//Counter: 5.
+//	Analysis ▼
+//	In this listing, you can see that three steps are occurring.First, the starting condition is
+//	set on line 8 : counter is initialized to 0. On line 10, the test of the condition occurs when
+//	counter is tested to see whether it is less than 5. Finally, the counter variable is incremented
+//	on line 12. This loop prints a simple message at line 13. As you can imagine,
+//	more important work could be done for each increment of the counter.
+//	A for loop combines the three steps into one statement.The three steps are initializing,
+//	testing, and incrementing.A for statement consists of the keyword for followed by a
+//	pair of parentheses.Within the parentheses are three statements separated by semicolons :
+//for (initialization; test; action)
+//{
+//	...
+//}
+//The first expression, initialization, is the starting conditions or initialization.Any
+//legal C++ statement can be put here, but typically this is used to create and initialize a
+//counting variable.The second expression, test, is the test, and any legal C++ expression
+//can be used here.This test serves the same role as the condition in the while loop.The
+//third expression, action, is the action that will take place.This action is typically the
+//Looping with the for Statement 181
+//7
+//LISTING 7.8 Continued
+//increment or decrement of a value, although any legal C++ statement can be put here.
