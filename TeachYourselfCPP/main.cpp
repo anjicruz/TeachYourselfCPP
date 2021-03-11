83947938654571d37f0551a0597682304bf45fbf
anjicruz <anji_cruz@msn.com>
2021-03-11 10:09:23 +0800

    Illustrating Nested for Loops 0311 1009am

--- TeachYourselfCPP/main.cpp
+++ TeachYourselfCPP/main.cpp
@@ -1,27 +1,59 @@
-﻿//	Empty for Loops, Illustrates the Null Statement in a for Loop 
+﻿//	Illustrating Nested for Loops
 
 #include <iostream>
 
-
 int main() 
-{	// using std::cout; using std::endl; using std::cin;
-	for (int i = 0; i < 5; std::cout << "i: " << i++ << std::endl);
+{	
+	using std::cout; using std::endl; using std::cin;
+	int rows, columns;
+	char theChar;
+	cout << "How many rows? ";
+	cin >> rows;
+	cout << "How many columns? ";
+	cin >> columns;
+	cout << "What character? ";
+	cin >> theChar;
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < columns; j++)
+			cout << theChar;
+		cout << endl;
+	}
 	return 0;
 }/*
 Output ▼
-i : 0
-	i : 1
-	i : 2
-	i : 3
-	i : 4
-	186 LESSON 7 : Controlling Program Flow
-	Analysis ▼
-	The for loop on line 8 includes three statements : The initialization statement establishes
-	the counter i and initializes it to 0. The condition statement tests for i<5, and the action
-	statement prints the value in i and increments it.
-	Nothing is left to do in the body of the for loop, so the null statement(;) is used.Note
-	that this is not a well - designed for loop : The action statement is doing far too much.
-	This would be better rewritten as
-	8 : for (int i = 0; i<5; i++)
-	9 : cout << “i : “ << i << endl;
-Although both do the same thing, this example is easier to understand.*/
+How many rows ? 4
+How many columns ? 12
+What character ? X
+XXXXXXXXXXXX
+XXXXXXXXXXXX
+XXXXXXXXXXXX
+XXXXXXXXXXXX
+Analysis ▼
+In this listing, the user is prompted for the number of rows and columns and for a character
+to print.The first for loop, on line 16, initializes a counter(i) to 0, and then the
+body of the outer for loop is run.
+On line 18, the first line of the body of the outer for loop, another for loop is established.
+A second counter(j) is initialized to 0, and the body of the inner for loop is executed.
+On line 19, the chosen character is printed, and control returns to the header of the
+inner for loop.Note that the inner for loop is only one statement(the printing of the
+	character).The condition is tested(j < columns) and if it evaluates true, j is incremented
+	and the next character is printed.This continues until j equals the number of
+	columns.
+	When the inner for loop fails its test, in this case after 12 Xs are printed, execution falls
+	through to line 20 and a new line is printed.The outer for loop now returns to its header,
+where its condition(i < rows) is tested.If this evaluates true, i is incremented and the
+body of the loop is executed.
+In the second iteration of the outer for loop, the inner for loop is started over.Thus, j is
+reinitialized to 0 and the entire inner loop is run again.
+The important idea here is that by using a nested loop, the inner loop is executed for each
+iteration of the outer loop.Thus, the character is printed columns times for each row.
+As an aside, many C++ programmers use the letters i and j as
+counting variables.This tradition goes all the way back to FORTRAN,
+in which the letters i, j, k, l, m, and n were the only counting
+variables.
+Although this might seem innocuous, readers of your program can
+become confused by the purpose of the counter and might use it
+improperly.You can even become confused in a complex program
+with nested loops.It is better to indicate the use of the index variable
+in its name—for instance, CustomerIndex or InputCounter.*/
