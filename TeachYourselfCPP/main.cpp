db088ce82b34b7fce68b074fccf68363997d5c41
anjicruz <anji_cruz@msn.com>
2021-03-16 16:19:08 +0800

    Creating a Stray Pointer 0316 418pm

--- TeachYourselfCPP/main.cpp
+++ TeachYourselfCPP/main.cpp
@@ -1,10 +1,23 @@
 ﻿//	LISTING 8.7 Creating a Stray Pointer
 
-
+typedef unsigned short int USHORT;
 #include <iostream>
 
 int main()
-{	//using std::cout; using std::endl; using std::cin;
-	// test
+{	using std::cout; using std::endl; using std::cin;
+	USHORT *pInt = new USHORT;
+	*pInt = 10;
+	cout << "*pInt: " << *pInt << endl;
+	delete pInt;
+
+	long * pLong = new long;
+	*pLong = 90000;
+	cout << "*pLong: " << *pLong << endl;
+
+	*pInt = 20; // uh oh, this was deleted
+
+	cout << "*pInt: " << *pInt << endl;
+	cout << "*pLong: " << *pLong << endl;
+	delete pLong;
 	return 0;
 }
