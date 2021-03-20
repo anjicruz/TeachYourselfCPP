de7a06d09de35e54aaada78fa5c38e4acdb815b8
anjicruz <anji_cruz@msn.com>
2021-03-20 18:42:51 +0800

    LISTING 9.6 swap() Rewritten with References 0320 642pm

--- TeachYourselfCPP/main.cpp
+++ TeachYourselfCPP/main.cpp
@@ -1,45 +1,57 @@
-﻿//Listing 9.4 - Demonstrates passing by value
+﻿//LISTING 9.6 swap() Rewritten with References
+
 #include <iostream>
 
-void swap(int x, int y);
-
+using namespace std;
+void swap(int &x, int &y);
 
 int main()
-{
-	using std::cout; using std::endl; using std::cin;
+{	//using std::cout; using std::endl; using std::cin;
 	int x = 5, y = 10;
+
 	cout << "Main. Before swap, x: " << x << " y: " << y << endl;
+
+
 	swap(x, y);
+
 	cout << "Main. After swap, x: " << x << " y: " << y << endl;
+
+
 	return 0;
 }
 
-void swap(int x, int y)
-{
-	using std::cout; using std::endl; using std::cin;
+void swap(int &rx, int &ry)
+{	//using std::cout; using std::endl; using std::cin;
 	int temp;
-	cout << "Swap. Before swap, x: " << x << " y: " << y << endl;
 
-	temp = x;
-	x = y;
-	y = temp;
+	cout << "Swap. Before swap, rx: " << rx << " ry: " << ry << endl;
 
-	cout << "Main. After swap, x: " << x << " y: " << y << endl;
+
+	temp = rx;
+	rx = ry;
+	ry = temp;
+
+
+	cout << "Swap. After swap, rx: " << rx << " ry: " << ry << endl;
+
 }/*
 Output ▼
-Main.Before swap, x: 5 y : 10
-Swap.Before swap, x : 5 y : 10
-Swap.After swap, x : 10 y : 5
-Main.After swap, x : 5 y : 10
-238 LESSON 9 : Exploiting References
+Main.Before swap, x:5 y : 10
+Swap.Before swap, rx : 5 ry : 10
+Swap.After swap, rx : 10 ry : 5
+Main.After swap, x : 10, y : 5
 Analysis ▼
-This program initializes two variables in main() and then passes them to the swap()
-function, which appears to swap them.When they are examined again in main(), they
-are unchanged!
-The problem here is that xand y are being passed to swap() by value.That is, local
-copies were made in the function.These local copies were changedand then thrown
-away when the function returnedand its local storage was deallocated.What is preferable
-is to pass xand y by reference, which changes the source values of the variable
-rather than a local copy.
-Two ways to solve this problem are possible in C++: You can make the parameters of
-swap() pointers to the original values or you can pass in references to the original values.*/
+Just as in the example with pointers, two variables are declared on line 10, and their values
+are printed on line 12. On line 15, the function swap() is called, but note that xand
+y, not their addresses, are passed.The calling function simply passes the variables.
+When swap() is called, program execution jumps to line 23, where the variables are
+identified as references.The values from the variables are printed on line 27, but note
+that no special operators are required.These variables are aliases for the original variables
+and can be used as such.
+On lines 30–32, the values are swapped, and then they’re printed on line 35. Program
+execution jumps back to the calling function, and on line 17, the values are printed in
+main().Because the parameters to swap() are declared to be references, the variables
+from main() are passed by reference, and thus their changed values are what is seen in
+main() as well.As you can see from this listing, references provide the convenienceand
+ease of use of normal variables, but with the powerand pass - by - reference capability of
+pointers!*/
