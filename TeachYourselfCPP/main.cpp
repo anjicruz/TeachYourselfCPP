5be0fb6fcbc1ca341d3e67cc4562d059b0da42f7
anjicruz <anji_cruz@msn.com>
2021-03-23 14:21:00 +0800

    LISTING 8.4 Array Pointer Relationship 0323 220pm

--- TeachYourselfCPP/main.cpp
+++ TeachYourselfCPP/main.cpp
@@ -1,26 +1,40 @@
-﻿// LISTING 8.3 Finding Out What Is Stored in Pointers
+﻿// LISTING 8.4 Array Pointer Relationship
 #include <iostream>
-
-
+const int ARRAY_LENGTH = 5;
 int main()
-{
-	using namespace std;
+{	using namespace std;
 	
-	unsigned short int myAge = 5, yourAge = 10;
+	// an array of 5 integers initialized to 5 values
+	int Numbers[ARRAY_LENGTH] = { 0,100,200,300,400 };
 
-	 // a pointer assigning pAge to myAge
-	unsigned short int* pAge = &myAge;
+	// pInt points to the first element
+	const int* pInt = Numbers;
 
-	cout << "myAge:\t\t" << myAge
-		<< "\t\tyourAge:\t" << yourAge << endl; // prints the value of the variable
+	cout << "Using a pointer to print the contents of the array: " << endl;
 
-	cout << "&myAge:\t\t" << &myAge
-		<< "\t&yourAge:\t" << &yourAge << endl; // prints the memory address
-
-	cout << "pAge:\t\t" << pAge
-		<< "\t*pAge:\t" << *pAge << endl; // pAge = pointer that holds the value of myAge
+	for (int nIndex = 0; nIndex < ARRAY_LENGTH; ++nIndex)
+		// cout << "Element[" << nIndex << "] = " << pInt[nIndex] << endl;
+		cout << "Element[" << nIndex << "] = " << *(pInt+nIndex) << endl;
 	return 0;
-}
+}/*
+Output ▼
+Using a pointer to print the contents of the array:
+Element[0] = 0
+Element[1] = 100
+Element[2] = 200
+Element[3] = 300
+Element[4] = 400
+214 LESSON 8: Pointers Explained
+Analysis ▼
+We created an array of five integers called Numbers and accessed its elements using the
+pointer pInt.Because an array name(Numbers in our case) is a pointer to the first element,
+pInt does just the same.Thus(pInt + 1) is a pointer to the second element, (pInt
+	+ 2) is a pointer to the third element, and so on.Dereferencing these pointers helps fetch
+	the values being pointed at.So, * pInt returns the first value, * (pInt + 1) the second
+	value, and so on.
+	The preceding example indicates the similarity between pointersand arrays.In fact, in
+	line 16, we could have simply used pInt[nIndex] instead of* (pInt + nIndex) and
+	still received the same output.*/
 
 
 
