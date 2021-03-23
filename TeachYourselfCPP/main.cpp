fd4b39cc224f662d28642cb36beb2ac2cd9457b1
anjicruz <anji_cruz@msn.com>
2021-03-23 09:00:21 +0800

    LISTING 8.3 Finding Out                                                       What Is Stored in Pointers 0323 900am

--- TeachYourselfCPP/main.cpp
+++ TeachYourselfCPP/main.cpp
@@ -1,32 +1,26 @@
-﻿// LISTING 8.2 Manipulating Data by Using Pointers
+﻿// LISTING 8.3 Finding Out What Is Stored in Pointers
 #include <iostream>
 
-typedef unsigned short int USHORT;
 
 int main()
 {
-
 	using namespace std;
 	
-	USHORT myAge; // a variable
-	USHORT* pAge; // a pointer
+	unsigned short int myAge = 5, yourAge = 10;
 
-	myAge = 5;
-	cout << "myAge: " << myAge << endl;
-	pAge = &myAge; // assign address of myAge to pAge
-	cout << "*pAge: " << *pAge << endl << endl;  // prints the pointer that holds the value stored at myAge
+	 // a pointer assigning pAge to myAge
+	unsigned short int* pAge = &myAge;
+
+	cout << "myAge:\t\t" << myAge
+		<< "\t\tyourAge:\t" << yourAge << endl; // prints the value of the variable
+
+	cout << "&myAge:\t\t" << &myAge
+		<< "\t&yourAge:\t" << &yourAge << endl; // prints the memory address
+
+	cout << "pAge:\t\t" << pAge
+		<< "\t*pAge:\t" << *pAge << endl; // pAge = pointer that holds the value of myAge
 	return 0;
-}/*
-Line 17: The indirection operator (*) in front of the pointer variable pAge means “the value
-stored at.” This assignment says, “Take the value stored at the address in pAge and
-assign it to myAge.”*/
-//
-//This program declares two variables : an unsigned short, myAge, and a pointer to an
-//unsigned short, pAge.myAge is assigned the value 5 on line 14; this is verified by the
-//printout on line 16.
-//On line 17, pAge is assigned the address of myAge.On line 18, pAge is dereferenced—
-//using the indirection operator (*)—and printed, showing that the value at the address that
-//pAge stores is the 5 stored in myAge.
+}
 
 
 
