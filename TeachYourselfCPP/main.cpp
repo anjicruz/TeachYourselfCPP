ad17c921ae44c40ed48dbd3d8b5d4be9bb78d196
anjicruz <anji_cruz@msn.com>
2021-03-26 06:58:34 +0800

    LISTING 9.2 Taking the Address of a Reference 0326 658am

--- TeachYourselfCPP/main.cpp
+++ TeachYourselfCPP/main.cpp
@@ -1,4 +1,4 @@
-﻿// Listing 9.1 - Demonstrating the use of references
+﻿// LISTING 9.2 Taking the Address of a Reference
 #include <iostream>
 
 int main()
@@ -11,26 +11,26 @@ int main()
 	cout << "intOne: " << intOne << endl;
 	cout << "rSomeRef: " << rSomeRef << endl; // prints rSomeRef.rSomeRef is a reference to an integer.The reference is initialized to refer to int.
 
-	rSomeRef = 7;
-	cout << "intOne: " << intOne << endl;
-	cout << "rSomeRef: " << rSomeRef << endl;
+	cout << "&intOne: " << &intOne << endl;
+	cout << "&rSomeRef: " << &rSomeRef << endl;
 	return 0;
 }/*
 Output ▼
 intOne : 5
 rSomeRef : 5
-intOne : 7
-rSomeRef : 7
+& intOne : 0x3500
+& rSomeRef : 0x3500
+234 LESSON 9 : Exploiting References
+Because the final two lines print memory addresses that might be
+unique to your computer or to a specific run of the program, your
+output might differ.
 Analysis ▼
-On line 8, a local integer variable, intOne, is declared.On line 9, a reference to an integer
-(int), rSomeRef, is declared and initialized to refer to intOne.As already stated, if
-you declare a reference but don’t initialize it, you receive a compile - time error.
-References must be initialized.
-On line 11, intOne is assigned the value 5. On lines 12 and 13, the values in intOneand
-rSomeRef are printed, and are, of course, the same.
-On line 15, 7 is assigned to rSomeRef.Because this is a reference, it is an alias for
-intOne, and thus the 7 is really assigned to intOne, as is shown by the printouts on lines
-16 and 17.*/
+rSomeRef is again initialized as a reference to intOne.This time, the addresses of the two
+variables are printed in lines 15 and 16, and they are identical.
+C++ gives you no way to access the address of the reference itself because it is not
+meaningful as it would be if you were using a pointer or other variable.References are
+initialized when created, and they always act as a synonym for their target, even when
+the address - of operator is applied.*/
 
 
 
