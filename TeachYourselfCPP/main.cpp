45ae45e0eb7fdbc977b71cc5d44c3d32c8b59c6a
anjicruz <anji_cruz@msn.com>
2021-03-27 16:11:54 +0800

    VS2019 Test Commit 0327 410pm

--- TeachYourselfCPP/main.cpp
+++ TeachYourselfCPP/main.cpp
@@ -1,26 +1,12 @@
-﻿
-// Listing 8.1 Demonstrates address-of operator
-// and addresses of local variables
-#include <iostream>
+﻿#include <iostream>
 
 int main()
+
 {
-	using namespace std;
-	unsigned short shortVar = 5;
-	unsigned long longVar = 65535;
-	long sVar = -65535;
 
-	cout << "shortVar:\t" << shortVar;
-	cout << "\tAddress of shortVar:\t";
-	cout << &shortVar << endl;
+	std::cout << "Hello, World!" << std::endl;   //endl = /n
 
-	cout << "longVar:\t" << longVar;
-	cout << "\tAddress of longVar:\t";
-	cout << &longVar << endl;
 
-	cout << "sVar:\t\t" << sVar;
-	cout << "\tAddress of sVar:\t";
-	cout << &sVar << endl;
+		return 0;
 
-	return 0;
 }
