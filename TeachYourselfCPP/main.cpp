b327b4c6d4b23e396940cbdc7e1d82d73a44c787
anjicruz <anji_cruz@msn.com>
2021-03-31 08:18:50 +0800

    LISTING 9.11 References as an Alternative 0331 818am

--- TeachYourselfCPP/main.cpp
+++ TeachYourselfCPP/main.cpp
@@ -1,4 +1,4 @@
-﻿//LISTING 9.10 Passing Pointer to a Constant Object
+﻿//LISTING 9.11 References as an Alternative
 
 #include <iostream>
 
@@ -33,35 +33,28 @@ SimpleCat::~SimpleCat()
 	cout << "Simple Cat destructor..." << endl;
 }
 
-const SimpleCat* const FunctionTwo(const SimpleCat * const theCat);
+const SimpleCat & FunctionTwo(const SimpleCat & theCat);
 
 
 int main()
 {
 	cout << "Making a cat..." << endl;
 	SimpleCat Frisky;
-	cout << "Frisky is ";
-	cout << Frisky.GetAge();
-	cout << " years old" << endl;
+	cout << "Frisky is " << Frisky.GetAge() << " years old" << endl ;
 	int age = 5;
 	Frisky.setAge(age);
-	cout << "Frisky is ";
-	cout << Frisky.GetAge();
-	cout << " years old" << endl;
-	cout << "Calling FunctionTwo..." << endl;
-	FunctionTwo(&Frisky);
-	cout << "Frisky is ";
-	cout << Frisky.GetAge();
-	cout << " years old" << endl;
+	cout << "Frisky is " << Frisky.GetAge() << " years old" << endl;
+	FunctionTwo(Frisky);
+	cout << "Frisky is " << Frisky.GetAge() << " years old" << endl;
 	return 0;
 }
 
-// FunctionTwo, passes by reference
-const SimpleCat* const FunctionTwo(const SimpleCat * const theCat)
+// FunctionTwo, passes a reference to a const object
+const SimpleCat & FunctionTwo(const SimpleCat & theCat)
 
 {
 	cout << "Function Two. Returning..." << endl;
-	cout << "Frisky is now " << theCat->GetAge();
+	cout << "Frisky is now " << theCat.GetAge();
 	cout << " years old" << endl;
 	// theCat->SetAge(8); const!
 	return theCat;
@@ -78,28 +71,14 @@ Frisky is 5 years old
 Simple Cat Destructor...
 
 Analysis ▼
-SimpleCat has added two accessor functions, GetAge() on line 13, which is a const
-function, and SetAge() on line 14, which is not a const function.It has also added the
-member variable, itsAge, on line 17.
-The constructor, copy constructor, and destructor are still defined to print their messages.
-The copy constructor is never called, however, because the object is passed by reference
-and so no copies are made.On line 42, an object is created, and its default age is printed,
-starting on line 43.
+The output is identical to that produced by Listing 9.10.The only significant change is
+that FunctionTwo() now takes and returns a reference to a constant object.Once again,
+working with references is somewhat simpler than working with pointers, and the same
+savingsand efficiency are achieved, as well as the safety provided by using const.
 
-On line 47, itsAge is set using the accessor SetAge, and the result is printed on line 48.
-FunctionOne is not used in this program, but FunctionTwo() is called.FunctionTwo()
-has changed slightly; the parameterand return value are now declared, on line 36, to take
-a constant pointer to a constant objectand to return a constant pointer to a constant
-object.
-
-Because the parameter and return value are still passed by reference, no copies are made
-and the copy constructor is not called.The object being pointed to in FunctionTwo(),
-however, is now constant, and thus cannot call the non - const method, SetAge().If the
-call to SetAge() on line 66 was not commented out, the program would not compile.
-
-LISTING 9.10 Continued
-Note that the object created in main() is not constant, and Frisky can call SetAge().
-The address of this nonconstant object is passed to FunctionTwo(), but because
-FunctionTwo()’s declaration declares the pointer to be a constant pointer to a constant
-object, the object is treated as if it were constant!*/
+C++ programmers do not usually differentiate between “constant reference to a
+SimpleCat object”and “reference to a constant SimpleCat object.” References
+themselves can never be reassigned to refer to another object, and so they are
+always constant.If the keyword const is applied to a reference, it is to make constant
+the object referred to.*/
 
