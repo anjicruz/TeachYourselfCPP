43bc00766df3d677b5d2301f08b9cdd737e11627
anjicruz <anji_cruz@msn.com>
2021-04-01 13:53:28 +0800

    Creating and Using References  0401 200pm

--- TeachYourselfCPP/main.cpp
+++ TeachYourselfCPP/main.cpp
@@ -9,9 +9,18 @@ int main()
 	intOne = 5;
 	std::cout << "intOne: " << intOne << std::endl;
 	std::cout << "rSomeRef: " << rSomeRef << std::endl;
-	intOne = 7;
-	std::cout << "intOne: " << intOne << std::endl;
-	std::cout << "rSomeRef: " << rSomeRef << std::endl;
+	// intOne = 7;
+	std::cout << "&intOne: " << &intOne << std::endl;
+	std::cout << "&rSomeRef: " << &rSomeRef << std::endl;
+	/*std::cout << "intOne: " << intOne << std::endl;
+	std::cout << "rSomeRef: " << rSomeRef << std::endl;*/
 	return 0;
-}
+}/*
+Analysis ▼
+rSomeRef is again initialized as a reference to intOne.This time, the addresses of the two
+variables are printed in lines 15 and 16, and they are identical.
+C++ gives you no way to access the address of the reference itself because it is not
+meaningful as it would be if you were using a pointer or other variable.References are
+initialized when created, and they always act as a synonym for their target, even when
+the address - of operator is applied.*/
 
