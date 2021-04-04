13917100579ce78115af5a5fa7d126ee11eeb501
anjicruz <anji_cruz@msn.com>
2021-04-04 08:37:15 +0800

    LISTING 9.7 Returning Values with Pointers 0404 837am

--- TeachYourselfCPP/main.cpp
+++ TeachYourselfCPP/main.cpp
@@ -1,43 +1,44 @@
-﻿// listing 9.6 swap() Rewritten with References
+﻿// LISTING 9.7 Returning Values with Pointers
 #include <iostream>
 
-using namespace std;
-
-void swap(int &x, int &y); 
-
-int main( )
-{     
-    int x = 5, y = 10; // Just as in the example with pointers, two variables are declared on line 10, and their values are printed on line 12. On line 15, the function swap() is called, but note that x and y, not their addresses, are passed.The calling function simply passes the variables.
-
-    cout << "Main. Before swap, x: " << x << " y: " << y << endl; 
 
 
-    swap(x,y);  
+short Factor(int n, int* pSquared, int* pCubed);
 
-    cout << "Main. After swap, x: " << x << " y: " << y << endl;
+int main()
+{
+	int number, squared, cubed; // On line 10, number, squared, and cubed are defined as short integers.
+	short error;
 
+	std::cout << "Enter a number(0-20): ";
+	std::cin >> number; // number is assigned a value based on user input on line 14.
 
-    return 0;
+	error = Factor(number, &squared, &cubed); // On line 16, this number and the addresses of squared and cubed are passed to the function Factor().
+
+	if (!error)
+	{
+		std::cout << "number: " << number << std::endl;
+		std::cout << "square: " << squared << std::endl;
+		std::cout << "cubed: " << cubed << std::endl;
+	}
+	else
+		std::cout << "Error encountered!!" << std::endl;
+	return 0;
 }
 
-void swap(int &rx, int &ry) // When swap() is called, program execution jumps to line 23, where the variables are identified as references.
-{
-    int temp;
+short Factor(int n, int* pSquared, int* pCubed)
+{ // On line 32, Factor() examines the first parameter, which is passed by value. If it is greater than 20 (the maximum value this function can handle), it sets the return value, Value, to a simple error value. Note that the return value from Function() is reserved for either this error value or the value 0, indicating all went well, and note that the function returns this value on line 40.
+	short Value = 0;
+	if (n > 20) 
+		Value = 1; // Error encountered!!
+	else
+	{
+		*pSquared = n * n;
+		*pCubed = n * n * n;
+		Value = 0; // value 0, indicating all went well!
+	}
+	return Value;
+}
 
-    cout << "Swap. Before swap, rx: " << rx << " ry: " << ry << endl; // The values from the variables are printed on line 27, but note that no special operators are required.These variables are aliases for the original variables and can be used as such.
-
-
-    temp = rx;
-    rx = ry;
-    ry = temp;
-
-
-    cout << "Swap. After swap, rx: " << rx << " ry: " << ry << endl;
-}/*
-On lines 30–32, the values are swapped, and then they’re printed on line 35. Program
-execution jumps back to the calling function, and on line 17, the values are printed in
-main().Because the parameters to swap() are declared to be references, the variables
-from main() are passed by reference, and thus their changed values are what is seen in
-main() as well.As you can see from this listing, references provide the convenience and
-ease of use of normal variables, but with the powerand pass - by - reference capability of
-pointers!*/
+//The actual values needed, the square and cube of number, are not returned by using the return mechanism; rather, they are returned by changing the pointers that were passed into the function.On lines 36 and 37, the pointers are assigned their return values.These values are assigned to the original variables by the use of indirection.You know this by the use of the dereference operator (*) with the pointer names.On line 38, Value is assigned a success value, and on line 40, it is returned.
+//Because passing by reference or by pointer allows uncontrolled minimum required for the function to do its job.This helps to ensure that the function is safer to useand more easily understandable.
