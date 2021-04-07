5494f05869e2fa8b35a4ac3f1f8dc75336b826ca
anjicruz <anji_cruz@msn.com>
2021-04-07 14:37:33 +0800

    Listing 9.9 - Passing pointers to objects 0407 230pm

--- TeachYourselfCPP/main.cpp
+++ TeachYourselfCPP/main.cpp
@@ -1,4 +1,4 @@
-﻿//Listing 9.9 - Passing pointers to objects
+﻿//Listing 9.9 - Passing pointers to objects  Although passing a pointer to FunctionTwo() is more efficient, it is dangerous. FunctionTwo() is not meant to be allowed to change the SimpleCat object it is passed, yet it is given the address of the SimpleCat.This seriously exposes the original object to change and defeats the protection offered in passing by value. Passing by value is like giving a museum a photograph of your masterpiece instead of the real thing.If vandals mark it up, there is no harm done to the original.Passing by reference is like sending your home address to the museum and inviting guests to come overand look at the real thing.The solution is to pass a pointer to a constant SimpleCat. Doing so prevents calling any non - const method on SimpleCat, and thus protects the object from change. Passing a constant reference allows your “guests” to see the original “painting, ” but not to alter it in any way.
 
 #include <iostream>
 
@@ -6,14 +6,21 @@ using namespace std;
 class Simplecat
 {
 public:
-	Simplecat(); // constructor
 	Simplecat(Simplecat&); // copy constructor
+	Simplecat(); // constructor
 	~Simplecat(); // destructor
+	// SimpleCat has added two accessor functions, GetAge() on line 13, which is a const function, and SetAge() on line 14, which is not a const function.It has also added the member variable, itsAge, on line 17.
+	int GetAge() const { return itsAge; }
+	void setAge(int age) { itsAge = age; }
+	// The constructor, copy constructor, and destructor are still defined to print their messages. The copy constructor is never called, however, because the object is passed by reference and so no copies are made.
+private:
+	int itsAge;
 };
-// The very simplified SimpleCat class is declared on lines 6–12. The constructor, copy constructor, and destructor all print an informative message so that you can tell when they’ve been called.
+
 Simplecat::Simplecat()
 {
 	cout << "Simple Cat Constructor..." << endl;
+	itsAge = 1; // Frisky is 1 years old
 }
 
 Simplecat::Simplecat(Simplecat&)
@@ -25,47 +32,57 @@ Simplecat::~Simplecat()
 {
 	cout << "Simple Cat Destructor..." << endl;
 }
-// Listing 9.9 creates the SimpleCat object and then calls two functions. The first function receives the Cat by value and then returns it by value.The second one receives a pointer to the object, rather than the object itself, and returns a pointer to the object.
-Simplecat FunctionOne(Simplecat theCat);
-Simplecat* FunctionTwo(Simplecat *theCat);
-// On line 34, main() prints out a message that is shown on the first line of the output. On line 35, a SimpleCat object is instantiated.This causes the constructor to be called, and the output from the constructor is seen on the second line of output. On line 36, main() reports that it is calling FunctionOne, which creates the third line of output.Because FunctionOne() is called passing the SimpleCat object by value, a copy of the SimpleCat object is made on the stack as an object local to the called function. This causes the copy constructor to be called, which creates the fourth line of output.
+
+const Simplecat * const FunctionTwo
+(const Simplecat * const theCat);
+// On line 42, an object is created, and its default age is printed, starting on line 43. On line 47, itsAge is set using the accessor SetAge, and the result is printed on line 48. FunctionOne is not used in this program, but FunctionTwo() is called.FunctionTwo() has changed slightly; the parameterand return value are now declared, on line 36, to take a constant pointer to a constant objectand to return a constant pointer to a constant object.
 int main()
 {
 	cout << "Making a cat..." << endl;
 	Simplecat Frisky;
-	cout << "Calling FunctionOne..." << endl;
-	FunctionOne(Frisky);
+	cout << "Frisky is ";
+	cout << Frisky.GetAge();
+	cout << " years old!" << endl;
+	int age = 5;
+	Frisky.setAge(age);
+	cout << "Frisky is ";
+	cout << Frisky.GetAge();
+	cout << " years old!" << endl;
 	cout << "Calling FunctionTwo..." << endl;
 	FunctionTwo(&Frisky);
+	cout << "Frisky is "; // Frisky is 5 years old!
+	cout << Frisky.GetAge();
+	cout << " years old!" << endl;
 	return 0;
 }
-// Program execution jumps to line 46 in the called function, which prints an informative message, the fifth line of output.The function then returns, and returns the SimpleCat object by value.This creates yet another copy of the object, calling the copy constructor and producing the sixth line of output.
-// FunctionOne, passes by value
-Simplecat FunctionOne(Simplecat theCat)
-{
-	cout << "Function One. Returning..." << endl;
-	return theCat;
-}
 
-// FunctionTwo, passes by reference
-Simplecat* FunctionTwo(Simplecat* theCat)
+// functionTwo, passes a const pointer
+const Simplecat* const FunctionTwo
+	(const Simplecat* const theCat)
 {
 	cout << "Function Two. Returning..." << endl;
+	cout << "Frisky is now " << theCat->GetAge();
+	cout << " years old!" << endl;
 	return theCat;
 }/*
 Output ▼
 Making a cat...
-Simple Cat Constructor...
-Calling FunctionOne...
-Simple Cat Copy Constructor...
-Function One.Returning...
-Simple Cat Copy Constructor...
-Simple Cat Destructor... the seventh line of output
-Simple Cat Destructor...
+Simple Cat constructor...
+Frisky is 1 years old
+Frisky is 5 years old
 Calling FunctionTwo...
-Function Two.Returning...
-Simple Cat Destructor...
+FunctionTwo.Returning...
+Frisky is now 5 years old
+Frisky is 5 years old
+Simple Cat Destructor...*/
 
-The return value from FunctionOne() is not assigned to any object, and so the temporary object created for the return is thrown away, calling the destructor, which produces the seventh line of output.Because FunctionOne() has ended, its local copy goes out of scope and is destroyed, calling the destructor and producing the eighth line of output. Program execution returns to main(), and FunctionTwo() is called, but the parameter is passed by reference.No copy is produced, so there’s no output.FunctionTwo() prints the message that appears as the tenth line of outputand then returns the SimpleCat object, again by reference, and so again produces no calls to the constructor or destructor. Finally, the program ends and Frisky goes out of scope, causing one final call to the destructor and printing the final line of output.The net effect of this is that the call to FunctionOne(), because it passed the Frisky by value, produced two calls to the copy constructorand two to the destructor, whereas the call to FunctionTwo() produced none.*/
+/* Because the parameter and return value are still passed by reference, no copies are made
+and the copy constructor is not called.The object being pointed to in FunctionTwo(),
+however, is now constant, and thus cannot call the non - const method, SetAge().If the
+call to SetAge() on line 66 was not commented out, the program would not compile.
 
+Note that the object created in main() is not constant, and Frisky can call SetAge().
+The address of this nonconstant object is passed to FunctionTwo(), but because
+FunctionTwo()’s declaration declares the pointer to be a constant pointer to a constant
+object, the object is treated as if it were constant!*/
 
