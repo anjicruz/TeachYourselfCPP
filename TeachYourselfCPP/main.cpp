c59d75383bb64e581ffd55dc678f458969b1d5fb
anjicruz <anji_cruz@msn.com>
2021-04-08 14:59:03 +0800

    LISTING 10.1 Accessing the Public Members of a Simple Class 0408 300pm

--- TeachYourselfCPP/main.cpp
+++ TeachYourselfCPP/main.cpp
@@ -1,40 +1,19 @@
-﻿// 9.12 Returning a Reference to a Nonexistent Object
+﻿// LISTING 10.1 Accessing the Public Members of a Simple Class
 #include <iostream>
-int& GetInt();
-
+class Cat
+{
+public:
+	int itsAge;    // member variable
+	int itsWeight; // member variable
+};
 int main()
 {
-	int& rInt = GetInt();
-	std::cout << "rInt= " << rInt << std::endl;
+	Cat Frisky;
+	Frisky.itsAge = 5; // assign to member variable
+	Frisky.itsWeight = 5;
+	std::cout << "Frisky is a cat who is " << Frisky.itsAge << " years old." << std::endl;
+	std::cout << "Frisky is a cat who weighs " << Frisky.itsWeight << " pounds." << std::endl;
+
 	return 0;
 }
-int & GetInt()
-{
-	int nLocalInt = 25;
-	return nLocalInt;
-}/*
-Returning Out - of - Scope Object References
-After C++ programmers learn to pass by reference, they have a tendency to go hog - wild.
-It is possible, however, to overdo it. Remember that a reference is always an alias to
-some other object.If you pass a reference into or out of a function, be certain to ask
-yourself, “What is the object I’m aliasing, and will it still exist every time it’s used ? ”
-Listing 9.12 illustrates the danger of returning a reference to an object that no longer
-exists.*/
-
-//
-//Output ▼
-//Compile error : Attempting to return a reference to a local object!
-// rInt = -858993460
-//
-//This program won’t compile on the Borland compiler.It will compile
-//on Microsoft compilers, perhaps with warnings; however, it
-//should be noted that it is a poor coding practice.
-//
-//The body of GetInt() declares a local object of type int and initializes its value to 25. It
-//then returns that local object by reference.Some compilers are smart enough to catch
-//this error and don’t let you run the program.Others let you run the program, with unpredictable
-//results.
-//When GetInt() returns, the local object, nLocalInt, is destroyed(painlessly, I assure
-//	you).The reference returned by this function is an alias to a nonexistent object, and this
-//	is a bad thing.
 
