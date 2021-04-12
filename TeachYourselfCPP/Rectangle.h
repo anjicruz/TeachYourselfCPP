8355826b55592f169c6069947471769c6c65de70
anjicruz <anji_cruz@msn.com>
2021-04-12 16:05:24 +0800

    On line 3, change class Point to struct Point. n On line 17, change class Rectangle to struct Rectangle.  0412 400pm

--- TeachYourselfCPP/Rectangle.h
+++ TeachYourselfCPP/Rectangle.h
@@ -1,6 +1,6 @@
 // Begin Rectangle.h Lines 3–14 in Rectangle.h (Listing 10.7) declare the class Point, which is used to hold a specific x - coordinate and y - coordinate on a graph.As written, this program doesn’t use Points much; however, other drawing methods require Points. LISTING 10.7 Declaring a Complete Class
 #include <iostream>
-class Point // holds x,y coordinates
+struct Point // holds x,y coordinates
 {
 	// no constructor, uses default
 public:
@@ -14,7 +14,7 @@ private:
 }; // Within the declaration of the class Point, you declare two member variables (itsX anditsY) on lines 12 and 13. These variables hold the values of the coordinates.As the xcoordinate increases, you move to the right on the graph.As the y - coordinate increases, you move upward on the graph.Other graphs use different systems.Some windowing programs, for example, increase the y - coordinate as you move down in the window.
 
 
-class Rectangle
+struct Rectangle
 {  // Line 17 begins the declaration of a Rectangle class. A Rectangle consists of four points that represent the corners of the Rectangle.The constructor for the Rectangle(line 20) takes four integers, known as top, left, bottom, and right.The four parameters to the constructor are copied into four member variables(Listing 10.8), and then the four Points are established.
 public:
 	Rectangle(int top, int left, int bottom, int right);
