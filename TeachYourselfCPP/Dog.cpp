fc63e8185243fc3ff1197a01816f0a29f2ba77c7
anjicruz <anji_cruz@msn.com>
2021-04-21 12:46:46 +0800

    LISTING 11.7 Calling a Base Method from a Overridden Method 0421 100pm

--- TeachYourselfCPP/Dog.cpp
+++ TeachYourselfCPP/Dog.cpp
@@ -1,36 +1,45 @@
-﻿// LISTING 11.5 Overriding a Base Class Method in a Derived Class
+﻿// LISTING 11.7 Calling a Base Method from a Overridden Method
 #include <iostream>
 using std::cout;
-// All the extra methods and data have been removed from these classes. On lines 8 and 9, the Mammal class declares the overloaded Move() methods.
+
 class Mammal
 {
 public:
 	void Move() const { cout << "Mammal move one step.\n"; }
 	void Move(int distance) const
 	{
-		cout << "Mammal move ";
-		cout << distance << " steps.\n";
+		cout << "Mammal move " << distance << " steps.\n";
 	}
 protected:
 	int itsAge;
 	int itsWeight;
 };
-// On line 22, Dog overrides the version of Move() with no parameters.
+
+
+
 class Dog : public Mammal
 {
 public:
-	void Move() const { cout << "Dog move 5 steps.\n"; }
+	void Move() const;
 };
 
-// These methods are invoked on lines 30–32, and the output reflects this as executed.
+void Dog::Move() const
+{
+	cout << "In dog move...\n";
+	Mammal::Move(3);
+}
+// On line 34, a Mammal, bigAnimal, is created, and on line 35, a Dog, Fido, is created. The method call on line 36 invokes the Move() method of Mammal, which takes an integer.
+// The programmer wanted to invoke Move(int) on the Dog object, but had a problem. Dog overrides the Move() method with no parameters, but does not overload the method that takes an integer—it does not provide a version that takes an integer.This is solved by the explicit call to the base class Move(int) method on line 37.
 int main()
 {
 	Mammal bigAnimal;
 	Dog Fido;
-	bigAnimal.Move();
 	bigAnimal.Move(2);
-	Fido.Move();
-	// Fido.Move(10);
+	Fido.Mammal::Move(6);
+	Fido.Move(); // I added this line to test. See the last 2 lines of the output
 	return 0;
-}
-// Line 33, however, is commented out because it causes a compile - time error.After you override one of the methods, you can no longer use any of the base methods of the same name. So, although the Dog class could have called the Move(int) method if it had not overridden the version of Move() without parameters, now that it has done so, it must override both if it wants to use both.Otherwise, it hides the method that it doesn’t override. This is reminiscent of the rule that if you supply any constructor, the compiler no longer supplies a default constructor. The rule is this : After you override any overloaded method, all the other overrides of that method are hidden.If you want them not to be hidden, you must override them all. It is a common mistake to hide a base class method when you intend to override it by forgetting to include the keyword const. const is part of the signature; leaving it off changes the signature, and thus hides the method rather than overrides it.
+}/*
+Mammal move 2 steps.
+Mammal move 6 steps.
+In dog move...
+Mammal move 3 steps.*/
