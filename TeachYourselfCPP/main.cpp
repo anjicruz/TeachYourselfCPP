733fb920b985ba5d3275a5612dff301b206170c7
anjicruz <anji_cruz@msn.com>
2021-05-20 21:05:47 +0800

    LISTING 12.5 Resolving Ambiguity in Case of Multiple Inheritance Involving Common Base Classes 0520 900PM

--- TeachYourselfCPP/main.cpp
+++ TeachYourselfCPP/main.cpp
@@ -1,49 +1,65 @@
-﻿// LISTING 12.4 Using Overloaded Base Class Constructors
-// Calling multiple constructors
+﻿// LISTING 12.5 Resolving Ambiguity in Case of Multiple Inheritance Involving Common Base Classes
+
 #include <iostream>
 using namespace std;
 
 typedef int HANDS;
 enum COLOR { Red, Green, Blue, Yellow, White, Black, Brown };
 
-class Horse
+class Animal
 {
 public:
-	Horse(COLOR color, HANDS height);
+	Animal(int);
+	virtual ~Animal() { cout << "Animal destructor...\n"; }
+	virtual int GetAge() const { return itsAge; }
+	virtual void SetAge(int age) { itsAge = age; }
+private:
+	int itsAge;
+};
+
+Animal::Animal(int age) :
+	itsAge(age)
+{
+	cout << "Animal constructor...\n";
+}
+
+class Horse : public Animal
+{
+public:
+	Horse(COLOR color, HANDS height, int age);
 	virtual ~Horse() { cout << "Horse destructor... "; }
 	virtual void Whinny() const { cout << "Whinny!... "; }
 	virtual HANDS GetHeight() const { return itsHeight; }
 	virtual COLOR GetColor() const { return  itsColor; }
-private:
+protected:
 	HANDS itsHeight;
 	COLOR itsColor;
 };
 
-Horse::Horse(COLOR color, HANDS height):
+Horse::Horse(COLOR color, HANDS height, int age):
+	Animal(age),
 itsColor(color),itsHeight(height)
 {
 	cout << "Horse constructor...\n";
 }
 
-class Bird
+class Bird : public Animal
 {
 public:
-	Bird(COLOR color, bool migrates);
+	Bird(COLOR color, bool migrates, int age);
 	virtual ~Bird() { cout << "Bird destructor..."; }
 	virtual void Chirp() const { cout << "Chirp..."; }
 	virtual void Fly() const
-	{
-		cout << "I can fly! I can fly! I can fly! ";
-	}
+	{ cout << "I can fly! I can fly! I can fly! "; }
 	virtual COLOR GetColor() const { return itsColor; }
 	virtual bool GetMigration() const { return itsMigration; }
-	
-private:
+protected:
 	COLOR itsColor;
 	bool itsMigration;
 };
 
-Bird::Bird(COLOR color, bool migrates):
+Bird::Bird(COLOR color, bool migrates, int age):
+	Animal(age),
 itsColor(color), itsMigration(migrates)
 {
 	cout << "Bird constructor...\n";
@@ -53,13 +69,12 @@ class Pegasus : public Horse, public Bird
 {
 public:
 	void Chirp() const { Whinny(); }
-	Pegasus(COLOR, HANDS, bool, long);
-	~Pegasus() { cout << "Pegasus destructor..."; }
+	Pegasus(COLOR, HANDS, bool, long, int);
+	virtual ~Pegasus() { cout << "Pegasus destructor..."; }
 	virtual long GetNumberBelievers() const
-	{
-		return itsNumberBelievers;
-	}
-
+	{ return itsNumberBelievers; }
+	virtual COLOR GetColor() const { return Horse::itsColor; }
+	virtual int GetAge() const { return Horse::GetAge(); }
 private:
 	long itsNumberBelievers;
 };
@@ -68,9 +83,10 @@ Pegasus::Pegasus(
 	COLOR aColor,
 	HANDS height,
 	bool migrates,
-	long NumBelieve):
-	Horse(aColor, height),
-	Bird(aColor, migrates),
+	long NumBelieve,
+	int age):
+	Horse(aColor, height, age),
+	Bird(aColor, migrates,age),
 	itsNumberBelievers(NumBelieve)
 {
 	cout << "Pegasus constructor...\n";
@@ -78,17 +94,9 @@ Pegasus::Pegasus(
 
 int main()
 {
-	Pegasus* pPeg = new Pegasus(Red, 5, true, 10);
-	pPeg->Fly();
-	pPeg->Whinny();
-	cout << "\nYour Pegasus is " << pPeg->GetHeight();
-	cout << " hands tall and ";
-	if (pPeg->GetMigration())
-		cout << "it does migrate.";
-	else
-		cout << "it does not migrate.";
-	cout << "\nA total of " << pPeg->GetNumberBelievers();
-	cout << " people believe it exists." << endl;
+	Pegasus* pPeg = new Pegasus (Red, 5, true, 10, 2);
+	int age = pPeg->GetAge();
+	cout << "This pegasus is " << age << " years old.\n";
 	delete pPeg;
 	return 0;
 }
