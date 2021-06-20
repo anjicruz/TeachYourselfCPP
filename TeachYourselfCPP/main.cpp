aa4a47178b7e8ef1adfc33032410d968368897d9
anjicruz <anji_cruz@msn.com>
2021-06-20 16:10:02 +0800

    Listing 13.3: How smart pointers that simulate normal pointer semantics 0620 4pm

--- TeachYourselfCPP/main.cpp
+++ TeachYourselfCPP/main.cpp
@@ -1,87 +1,99 @@
-﻿// LISTING 13.1 A Calendar Class That Handles Day, Month and Year, and Allows Increments in Days
+﻿// Listing 13.3: How smart pointers that simulate normal pointer semantics
+#include <memory>
 #include <iostream>
-class CDate
+using namespace std;
+
+class Dog
 {
-private:
-	int m_nDay; // range 1-30
-	int m_nMonth; // range 1-12
-	int m_nYear;
-
-	void AddDays(int nDaysToAdd)
-	{
-		m_nDay += nDaysToAdd;
-
-		{
-			if (m_nDay > 30)
-				AddMonths(m_nDay / 30);
-			m_nDay %= 30;
-		}
-	}
-
-
-	void AddMonths(int nMonthsToAdd)
-	{
-		m_nMonth += nMonthsToAdd;
-
-		if (m_nMonth > 12)
-		{
-			AddYears(m_nMonth / 12);
-
-			m_nMonth %= 12;  // rollover dec -> jan
-		}
-	}
-
-	void AddYears(int m_nYearsToAdd)
-	{
-		m_nYear += m_nYearsToAdd;
-	}
-
 public:
+	Dog() {};
+	~Dog() {};
 
-	// constructor that initializes the object to a day, month and year
-	CDate(int nDay, int nMonth, int nYear)
-		: m_nDay(nDay), m_nMonth(nMonth), m_nYear(nYear) {};
-
-	// unary increment operator(prefix)
-	CDate& operator ++ ()
+	void Bark()
 	{
-		AddDays(1);
-		return *this;
-	}
-
-	// postfix 
-	CDate operator ++ (int)
-	{
-
-		CDate mReturnDate(m_nDay, m_nMonth, m_nYear);
-
-		AddDays(1);
-		// IncrementDay();
-
-		return mReturnDate;
-	}
-
-	void DisplayDate()
-	{
-		std::cout << m_nDay << " / " << m_nMonth << " / " << m_nYear;
+		cout << "Bark! Bark!" << endl;
 	}
 };
 
 int main()
 {
+	auto_ptr<int> pSmartIntPtr(new int);
+	*pSmartIntPtr = 25;
+	cout << "*pSmartIntPtr = " << *pSmartIntPtr << endl;
+	auto_ptr// LISTING 13.6 Calendar Class Featuring the Binary Addition Operator
+#include <iostream>
+		class CDate
+	{
+	private:
+		int m_nDay; // range 1-30
+		int m_nMonth; // range 1-12
+		int m_nYear;
 
-	CDate mDate(25, 6, 2008);
+		void AddDays(int nDaysToAdd)
+		{
+			m_nDay += nDaysToAdd;
 
-	std::cout << "The date object is initialized to: ";
+			if (m_nDay > 30)
+			{
+				AddMonths(m_nDay / 30);
 
-	mDate.DisplayDate();
-	std::cout << std::endl;
+				m_nDay %= 30;
+			}
+		}
 
-	++mDate;
+		void AddMonths(int nMonthsToAdd)
+		{
+			m_nMonth += nMonthsToAdd;
 
-	std::cout << "Date after prefix increment is: ";
+			if (m_nMonth > 12)
+			{
+				AddYears(m_nMonth / 12);
 
-	mDate.DisplayDate();
-	std::cout << std::endl;
+				m_nMonth %= 12;
+			}
+		}
+
+		void AddYears(int m_nYearsToAdd)
+		{
+			m_nYear += m_nYearsToAdd;
+		}
+
+	public:
+
+		// constructor that initializes the object to a day, month and year
+		CDate(int nDay, int nMonth, int nYear)
+			: m_nDay(nDay), m_nMonth(nMonth), m_nYear(nYear) {};
+
+		CDate operator + (int nDaysToAdd)
+		{
+			CDate newDate(m_nDay, m_nMonth, m_nYear);
+			newDate.AddDays(nDaysToAdd);
+			return newDate;
+		}
+		void DisplayDate()
+		{
+			std::cout << m_nDay << " / " << m_nMonth << " / " << m_nYear;
+		}
+	};
+
+	int main()
+	{
+
+		CDate mDate(25, 6, 2008);
+
+		std::cout << "The date object is initialized to: ";
+
+		mDate.DisplayDate();
+		std::cout << std::endl;
+
+		std::cout << "Date after adding 10 days is: ";
+
+		CDate datePlus10(mDate + 10);
+		datePlus10.DisplayDate();
+		return 0;
+	}
+	<Dog> pSmartDog(new Dog);
+	pSmartDog->Bark();
+
 	return 0;
 }
