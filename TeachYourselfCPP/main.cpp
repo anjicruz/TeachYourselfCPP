f298063297f6e6b979386e68b7b73f4ea9715434
anjicruz <anji_cruz@msn.com>
2021-06-20 16:29:00 +0800

    LISTING 13.6 Calendar Class Featuring the Binary Addition Operator 0620 428PM

--- TeachYourselfCPP/main.cpp
+++ TeachYourselfCPP/main.cpp
@@ -1,4 +1,4 @@
-﻿// LISTING 13.5 Using Conversion Operators to Convert a CDate into an Integer
+﻿// LISTING 13.6 Calendar Class Featuring the Binary Addition Operator
 #include <iostream>
 class CDate
 {
@@ -7,15 +7,45 @@ private:
     int m_nMonth; // range 1-12
     int m_nYear;
 
+    void AddDays(int nDaysToAdd)
+    {
+        m_nDay += nDaysToAdd;
+
+        if (m_nDay > 30)
+        {
+            AddMonths(m_nDay / 30);
+
+            m_nDay %= 30;
+        }
+    }
+
+    void AddMonths(int nMonthsToAdd)
+    {
+        m_nMonth += nMonthsToAdd;
+
+        if (m_nMonth > 12)
+        {
+            AddYears(m_nMonth / 12);
+
+            m_nMonth %= 12;
+        }
+    }
+
+    void AddYears(int m_nYearsToAdd)
+    {
+        m_nYear += m_nYearsToAdd;
+    }
+
 public:
     // constructor that initializes the object to a day, month and year
     CDate(int nDay, int nMonth, int nYear)
         : m_nDay(nDay), m_nMonth(nMonth), m_nYear(nYear) {};
 
-    // Convert date object into an integer
-    operator int()
+    CDate operator+(int nDaysToAdd)
     {
-        return ((m_nYear * 10000) + (m_nMonth * 100) + m_nDay);
+        CDate newDate(m_nDay, m_nMonth, m_nYear);
+        newDate.AddDays(nDaysToAdd);
+        return newDate;
     }
     void DisplayDate()
     {
@@ -33,7 +63,9 @@ int main()
     mDate.DisplayDate();
     std::cout << std::endl;
 
-    int nDate = mDate;
-    std::cout << "The integer equivalent of the date is: " << nDate;
+    std::cout << "Date after adding 10 days is: ";
+
+    CDate datePlus10(mDate + 10);
+    datePlus10.DisplayDate();
     return 0;
 }
