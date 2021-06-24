c8ce63e8e219ff04c00ebcf3ba96acb2b84f2f5e
anjicruz <anji_cruz@msn.com>
2021-06-24 11:04:26 +0800

    LISTING 10.1 Accessing the Public Members of a Simple Class June24 11AM

--- TeachYourselfCPP/main.cpp
+++ TeachYourselfCPP/main.cpp
@@ -1,71 +1,15 @@
-﻿// LISTING 13.6 Calendar Class Featuring the Binary Addition Operator
+﻿// LISTING 10.1 Accessing the Public Members of a Simple Class
 #include <iostream>
-class CDate
-{
-private:
-    int m_nDay;   // range 1-30
-    int m_nMonth; // range 1-12
-    int m_nYear;
 
-    void AddDays(int nDaysToAdd)
-    {
-        m_nDay += nDaysToAdd;
-
-        if (m_nDay > 30)
-        {
-            AddMonths(m_nDay / 30);
-
-            m_nDay %= 30;
-        }
-    }
-
-    void AddMonths(int nMonthsToAdd)
-    {
-        m_nMonth += nMonthsToAdd;
-
-        if (m_nMonth > 12)
-        {
-            AddYears(m_nMonth / 12);
-
-            m_nMonth %= 12;
-        }
-    }
-
-    void AddYears(int m_nYearsToAdd)
-    {
-        m_nYear += m_nYearsToAdd;
-    }
-
+class Cat {
 public:
-    // constructor that initializes the object to a day, month and year
-    CDate(int nDay, int nMonth, int nYear)
-        : m_nDay(nDay), m_nMonth(nMonth), m_nYear(nYear) {};
-
-    CDate operator+(int nDaysToAdd)
-    {
-        CDate newDate(m_nDay, m_nMonth, m_nYear);
-        newDate.AddDays(nDaysToAdd);
-        return newDate;
-    }
-    void DisplayDate()
-    {
-        std::cout << m_nDay << " / " << m_nMonth << " / " << m_nYear;
-    }
+    int itsAge;
+    int itsWeight;
 };
-
 int main()
 {
-
-    CDate mDate(25, 6, 2008);
-
-    std::cout << "The date object is initialized to: ";
-
-    mDate.DisplayDate();
-    std::cout << std::endl;
-
-    std::cout << "Date after adding 10 days is: ";
-
-    CDate datePlus10(mDate + 10);
-    datePlus10.DisplayDate();
+    Cat Frisky;
+    Frisky.itsAge = 5;
+    std::cout << "Frisky is " << Frisky.itsAge << " years old" << std::endl;
     return 0;
 }
