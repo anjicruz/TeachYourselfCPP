7fae63d986c599301338f6f6b589a1dc3587ac27
anjicruz <anji_cruz@msn.com>
2021-07-07 11:34:08 +0800

    Listing 11.3 Constructors and destructors called July7 1130am

--- TeachYourselfCPP/Main.cpp
+++ TeachYourselfCPP/Main.cpp
@@ -1,16 +1,14 @@
-﻿// Listing 11.2 Using a derived object
+﻿// Listing 11.3 Constructors and destructors called.
 #include <iostream>
-using std::cout;
-using std::endl;
-
+using namespace std;
 enum BREED { GOLDEN, CAIRN, DANDIE, SHETLAND, LAB};
 
 class Mammal
 {
 public:
 	// constructors
-	Mammal():itsAge(2), itsWeight(5){}
-	~Mammal(){}
+	Mammal();
+	~Mammal();
 
 	// accessors
 	int GetAge() const { return itsAge; }
@@ -32,8 +30,8 @@ class Dog :public Mammal
 public:
 
 	// constructors
-	Dog():itsBreed(GOLDEN){}
-	~Dog(){}
+	Dog();
+	~Dog();
 
 	// accessors
 	BREED GetBreed() const { return itsBreed; }
@@ -47,6 +45,28 @@ private:
 	BREED itsBreed;
 };
 
+Mammal::Mammal():
+	itsAge(3),
+	itsWeight(5)
+{
+	std::cout << "Mammal constructor..." << endl;
+}
+
+Mammal::~Mammal()
+{
+	std::cout << "Mammal destructor..." << endl;
+}
+
+Dog::Dog() :
+	itsBreed(GOLDEN)
+{
+	std::cout << "Dog constructor..." << endl;
+}
+
+Dog::~Dog()
+{
+	std::cout << "Dog destructor..." << endl;
+}
 int main() {
 	Dog Fido;
 	Fido.Speak();
