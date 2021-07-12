214880f6628e2d3522c2a26fa08c5a6e49a8bbfe
anjicruz <anji_cruz@msn.com>
2021-07-12 10:35:49 +0800

    LISTING 11.4 Overloading Constructors in Derived Classes July 12 1030AM

--- TeachYourselfCPP/Main.cpp
+++ TeachYourselfCPP/Main.cpp
@@ -2,19 +2,15 @@
 #include <iostream>
 using std::cout;
 
-enum BREED { GOLDEN, CAIRN, DANDIE, SHETLAND, DOBERMAN, LAB};
-
 class Mammal
 {
 public:
-	// constructors
-	Mammal() { cout << "Mammal constructor...\n"; }
-	~Mammal() { cout << "Mammal destructor...\n"; }
-	
-	// other methods
-	void Speak() const { cout << "Mammal Sound!\n"; }
-	void Sleep() const { cout << "Shhh. I'm sleeping.\n"; }
-
+	void Move() const { cout << "Mammal move one step.\n"; }
+	void Move(int distance) const
+	{
+		cout << "Mammal move";
+		cout << distance << " steps.\n";
+	}
 protected:
 	int itsAge;
 	int itsWeight;
@@ -23,24 +19,17 @@ protected:
 class Dog :public Mammal
 {
 public:
-	// constructors
-	Dog() { cout << "Dog constructor...\n"; }
-	~Dog() { cout << "Dog destructor...\n"; }
-	
-	// other methods
-	void WagTail() const { cout << "Tail Wagging...\n"; }
-	void BegForFood() const { cout << "Begging for food...\n"; }
-	void Speak() const { cout << "Woof!\n"; }
-
-private:
-	BREED itsBreed;
+	// You might receive a warning that you are hiding a function
+	void Move() const { cout << "Dog move 5 steps.\n";}
 };
-
 
 int main() {
 	Mammal bigAnimal;
 	Dog Fido;
-	bigAnimal.Speak();
-	Fido.Speak();
+	bigAnimal.Move();
+	bigAnimal.Move(2);
+	Fido.Move();
+	// Fido.Move(10);
+
 	return 0;
 }
